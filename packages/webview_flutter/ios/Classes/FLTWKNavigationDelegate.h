3a04374bb92c4c900ad0dfd624995f162389ed67
Kosuke Kito <blueberryjam09@gmail.com>
2019-07-19 11:14:22 +0900

    Reformat FLTWKNavigationDelegate.h

--- packages/webview_flutter/ios/Classes/FLTWKNavigationDelegate.h
+++ packages/webview_flutter/ios/Classes/FLTWKNavigationDelegate.h
@@ -1,21 +1,21 @@
-// Copyright 2019 The Chromium Authors. All rights reserved.	
-// Use of this source code is governed by a BSD-style license that can be	
-// found in the LICENSE file.	
+// Copyright 2019 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
 
-#import <Flutter/Flutter.h>	
-#import <WebKit/WebKit.h>	
+#import <Flutter/Flutter.h>
+#import <WebKit/WebKit.h>
 
-NS_ASSUME_NONNULL_BEGIN	
+NS_ASSUME_NONNULL_BEGIN
 
-@interface FLTWKNavigationDelegate : NSObject <WKNavigationDelegate>	
+@interface FLTWKNavigationDelegate : NSObject <WKNavigationDelegate>
 
 - (instancetype)initWithChannel:(FlutterMethodChannel*)channel AndArgs:(id _Nullable)args;
 
-/**	
- * Whether to delegate navigation decisions over the method channel.	
- */	
-@property(nonatomic, assign) BOOL hasDartNavigationDelegate;	
+/**
+ * Whether to delegate navigation decisions over the method channel.
+ */
+@property(nonatomic, assign) BOOL hasDartNavigationDelegate;
 
-@end	
+@end
 
 NS_ASSUME_NONNULL_END
